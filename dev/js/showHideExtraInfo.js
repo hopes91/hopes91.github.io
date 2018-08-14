a412f456ecd252209ea67067169397a6925f806c
hopes91 <nadya.me91@gmail.com>
2018-08-14 10:45:51 +0300

    finish showHideExtraInfo.js

--- dev/js/showHideExtraInfo.js
+++ dev/js/showHideExtraInfo.js
@@ -5,11 +5,16 @@ const portfolioSection = document.getElementById('portfolio');
 const closeIcon = document.getElementById('close');
 
 function showInfo(event) {
-	console.log(event.target);
+	console.log(event.target.className);
 	back.style.display = 'block';
+
+	if (event.target.className === 'about') aboutSection.style.display = 'block';
+	else portfolioSection.style.display = 'block';
 }
 
 function hideInfo() {
+	if (aboutSection.style.display = 'block') aboutSection.style.display = 'none';
+	else portfolioSection.style.display = 'none';
 	back.style.display = 'none';
 }
 
