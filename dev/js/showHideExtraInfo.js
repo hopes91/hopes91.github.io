36db93b125c11d86f1fcee55abdbf675fd26a41e
hopes91 <nadya.me91@gmail.com>
2018-08-16 16:36:03 +0300

    open and close the projects properly

--- dev/js/showHideExtraInfo.js
+++ dev/js/showHideExtraInfo.js
@@ -3,19 +3,49 @@ const back = document.getElementById('back');
 const front = document.getElementById('front');
 const aboutSection = document.getElementById('about');
 const portfolioSection = document.getElementById('portfolio');
+const projects = document.querySelectorAll('.project');
+const projectsInfo = document.getElementById('projects-info');
+const projectContent = document.querySelectorAll('.project__content');
+const goBack = document.querySelectorAll('.go-back');
 const closeIcon = document.getElementById('close');
 
 function showInfo(event) {
 	back.style.zIndex = '10';
 	front.style.display = 'block';
 
-	if (event.target.className === 'about') {
+	if (this.id === 'nav' && projectsInfo.style.display === 'block') {
+		aboutSection.style.display = 'none';
+		portfolioSection.style.display = 'none';
+	} else if (event.target.className === 'about') {
 		portfolioSection.style.display = 'none';
 		aboutSection.style.display = 'block';
 	} else {
 		aboutSection.style.display = 'none';
 		portfolioSection.style.display = 'flex';
 	}
+}
+
+function showProject() {
+	portfolioSection.style.display = 'none';
+	closeIcon.style.display = 'none';
+	projectsInfo.style.display = 'block';
+
+	let clickedThumb = this.classList[1];
+
+	projectContent.forEach(project => {
+		let comparedProject = project.classList[1];
+
+		if (clickedThumb === comparedProject) {
+			project.style.display = 'block';
+		}
+	});
+}
+
+function goToPortfolio() {
+	projectContent.forEach(project => project.style.display = 'none');
+	projectsInfo.style.display = 'none';
+	closeIcon.style.display = 'block';
+	portfolioSection.style.display = 'flex';
 }
 
 function hideInfo() {
@@ -26,5 +56,7 @@ function hideInfo() {
 }
 
 navbar.forEach(link => link.addEventListener('click', showInfo));
+projects.forEach(project => project.addEventListener('click', showProject));
+goBack.forEach(back => back.addEventListener('click', goToPortfolio));
 closeIcon.addEventListener('click', hideInfo);
 
