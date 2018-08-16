59992b7f342b97b2932ce5c2ff983dcf1ff690dc
hopes91 <nadya.me91@gmail.com>
2018-08-16 10:25:16 +0300

    a few styling changes of the index page

--- dev/js/showHideExtraInfo.js
+++ dev/js/showHideExtraInfo.js
@@ -1,10 +1,12 @@
 const navbar = document.querySelectorAll('#nav');
+const back = document.getElementById('back');
 const front = document.getElementById('front');
 const aboutSection = document.getElementById('about');
 const portfolioSection = document.getElementById('portfolio');
 const closeIcon = document.getElementById('close');
 
 function showInfo(event) {
+	back.style.zIndex = '10';
 	front.style.display = 'block';
 
 	if (event.target.className === 'about') {
@@ -20,6 +22,7 @@ function hideInfo() {
 	aboutSection.style.display = 'none';
 	portfolioSection.style.display = 'none';
 	front.style.display = 'none';
+	back.style.zIndex = '0';
 }
 
 navbar.forEach(link => link.addEventListener('click', showInfo));
