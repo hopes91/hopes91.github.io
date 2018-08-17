9c776b7b4d68f02379291ceef492c0a2f9db00db
hopes91 <nadya.me91@gmail.com>
2018-08-17 15:38:37 +0300

    add comments

--- dev/js/showHideExtraInfo.js
+++ dev/js/showHideExtraInfo.js
@@ -9,11 +9,13 @@ const projectContent = document.querySelectorAll('.project__content');
 const goBack = document.querySelectorAll('.go-back');
 const closeIcon = document.getElementById('close');
 
+// open the div in which you can look at the "about me" and "portfolio" sections, and also all of my projects
 function showInfo(event) {
 	back.style.zIndex = '10';
 	front.style.display = 'block';
 
-	if (this.id === 'nav' && projectsInfo.style.display === 'block') {
+	if (this.id === 'nav' && projectsInfo.style.display === 'block') { // if any project is opened
+		// do not show the about or portfolio sections
 		aboutSection.style.display = 'none';
 		portfolioSection.style.display = 'none';
 	} else if (event.target.className === 'about') {
@@ -25,12 +27,14 @@ function showInfo(event) {
 	}
 }
 
+
+// open the whole project after you clicked the project card
 function showProject() {
 	portfolioSection.style.display = 'none';
 	closeIcon.style.display = 'none';
 	projectsInfo.style.display = 'block';
 
-	let clickedThumb = this.classList[1];
+	let clickedThumb = this.classList[1]; // the class is the unic name of the project
 
 	projectContent.forEach(project => {
 		let comparedProject = project.classList[1];
@@ -41,6 +45,8 @@ function showProject() {
 	});
 }
 
+
+// slider
 let index = 1;
 
 function showSlides(n) {
@@ -49,8 +55,8 @@ function showSlides(n) {
 
 	projectContent.forEach(project => {
 		if (project.style.display === 'block') {
-			images = project.children[2].children;
-			dots = project.children[3].children;
+			images = project.children[2].children; // the images of the active (opened) project
+			dots = project.children[3].children; // the dots of the active (opened) project
 		}
 	});
 
@@ -76,6 +82,8 @@ function currentSlide(n) {
 	showSlides(index = n);
 }
 
+
+// close the opened project and go back to the portfolio section
 function goToPortfolio() {
 	projectContent.forEach(project => project.style.display = 'none');
 	projectsInfo.style.display = 'none';
@@ -83,6 +91,8 @@ function goToPortfolio() {
 	portfolioSection.style.display = 'flex';
 }
 
+
+// close the div in which you can look at the "about me" and "portfolio" sections, and also all of my projects
 function hideInfo() {
 	aboutSection.style.display = 'none';
 	portfolioSection.style.display = 'none';
