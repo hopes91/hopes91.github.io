1d60af6126146915bfca0159c18b18f17ae12184
hopes91 <nadya.me91@gmail.com>
2018-08-29 15:57:27 +0300

    make the animation start earlier

--- dev/js/moveElements.js
+++ dev/js/moveElements.js
@@ -8,21 +8,21 @@ const photoCopyright = document.querySelector('.photo-copyright');
 function moveElements() {
 	setTimeout(function() {
 		siteName.style.left = '0';
-	}, 1000);
+	}, 0);
 
 	setTimeout(function() {
 		nav.style.left = '0';
-	}, 1500);
+	}, 500);
 
 	setTimeout(function() {
 		contacts.style.bottom = '0';
 		languages.style.top = '0';
-	}, 2000);
+	}, 1000);
 
 	setTimeout(function() {
 		stylingElement.style.opacity = '1';
 		photoCopyright.style.opacity = '1';
-	}, 4000);
+	}, 3000);
 }
 
 window.addEventListener('load', moveElements);
