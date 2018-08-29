9516fcf958b3a6d5d65879e6bf61f058c09e2e52
hopes91 <nadya.me91@gmail.com>
2018-08-29 16:53:13 +0300

    hide animation

--- dev/js/moveElements.js
+++ dev/js/moveElements.js
@@ -1,29 +1,29 @@
-const siteName = document.getElementById('name');
-const languages = document.getElementById('languages');
-const nav = document.getElementById('nav');
-const contacts = document.getElementById('contacts');
-const stylingElement = document.querySelector('.styling-element');
-const photoCopyright = document.querySelector('.photo-copyright');
+// const siteName = document.getElementById('name');
+// const languages = document.getElementById('languages');
+// const nav = document.getElementById('nav');
+// const contacts = document.getElementById('contacts');
+// const stylingElement = document.querySelector('.styling-element');
+// const photoCopyright = document.querySelector('.photo-copyright');
 
-function moveElements() {
-	setTimeout(function() {
-		siteName.style.left = '0';
-	}, 0);
+// function moveElements() {
+// 	setTimeout(function() {
+// 		siteName.style.left = '0';
+// 	}, 0);
 
-	setTimeout(function() {
-		nav.style.left = '0';
-	}, 500);
+// 	setTimeout(function() {
+// 		nav.style.left = '0';
+// 	}, 500);
 
-	setTimeout(function() {
-		contacts.style.bottom = '0';
-		languages.style.top = '0';
-	}, 1000);
+// 	setTimeout(function() {
+// 		contacts.style.bottom = '0';
+// 		languages.style.top = '0';
+// 	}, 1000);
 
-	setTimeout(function() {
-		stylingElement.style.opacity = '1';
-		photoCopyright.style.opacity = '1';
-	}, 3000);
-}
+// 	setTimeout(function() {
+// 		stylingElement.style.opacity = '1';
+// 		photoCopyright.style.opacity = '1';
+// 	}, 3000);
+// }
 
-window.addEventListener('load', moveElements);
+// window.addEventListener('load', moveElements);
 
