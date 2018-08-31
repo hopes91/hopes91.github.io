8b65b54eeacad9b7288990536f14bc1f26c42c13
hopes91 <nadya.me91@gmail.com>
2018-08-31 10:33:31 +0300

    make the code more readable

--- dev/js/scrollToAnchor.js
+++ dev/js/scrollToAnchor.js
@@ -3,24 +3,35 @@ const anchorElements = document.querySelectorAll('[href^="#"]');
 function scrollToAnchor(event) {
 	event.preventDefault();
 
-	let speed = 1; // scrolling speed
+	let speed = 1;
 	let y = window.pageYOffset;
-	let hash = this.href.replace(/[^#]*(.*)/, '$1'); // the id of the element
-	let indent = document.querySelector(hash).getBoundingClientRect().top; // the indent from the top of the browser's window to the id
+	let hash = this.href.replace(/[^#]*(.*)/, '$1');
+	let indent = document.querySelector(hash).getBoundingClientRect().top;
 	let start = null;
 
 	requestAnimationFrame(step);
 
 	function step(time) {
-		if (start === null) start = time;
+		if (start === null) {
+			start = time;
+		}
 		
 		let progress = time - start;
-		let scroll = (indent < 0 ? Math.max(y - progress/speed, y + indent) : Math.min(y + progress/speed, y + indent));
+		let scroll = null;
+
+		if (indent < 0) {
+			scroll = Math.max(y - progress/speed, y + indent);
+		} else {
+			scroll = Math.min(y + progress/speed, y + indent);
+		}
 
 		window.scrollTo(0, scroll);
 
-		if (scroll !== y + indent) requestAnimationFrame(step);
-		else location.hash = hash;
+		if (scroll !== y + indent) {
+			requestAnimationFrame(step);
+		} else {
+			location.hash = hash;
+		}
 	}
 }
 
