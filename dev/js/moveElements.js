4f3c779ba0aad9d5cda52058bfbac339fd94e1d7
hopes91 <nadya.me91@gmail.com>
2018-11-08 22:10:03 +0400

    fix animation

--- dev/js/moveElements.js
+++ dev/js/moveElements.js
@@ -1,6 +1,8 @@
 const siteName = document.getElementById('name');
 const languages = document.getElementById('languages');
+const nav = document.getElementById('nav');
 const contacts = document.getElementById('contacts');
+const copyright = document.querySelector('.photo-copyright');
 
 function moveElements() {
   setTimeout(function() {
@@ -12,9 +14,13 @@ function moveElements() {
   }, 500);
 
   setTimeout(function() {
-    contacts.style.right = '0';
+    contacts.style.left = '0';
     languages.style.top = '0';
   }, 1000);
+
+  setTimeout(function() {
+    copyright.style.opacity = '1';
+  }, 2000);
 }
 
 window.addEventListener('load', moveElements);
