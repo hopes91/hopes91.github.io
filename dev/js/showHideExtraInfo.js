7170bd6840ae48fb3481a768b259684b79bc8fe4
hopes91 <nadya.me91@gmail.com>
2019-04-10 17:23:42 +0300

    add working tabindexes

--- dev/js/showHideExtraInfo.js
+++ dev/js/showHideExtraInfo.js
@@ -1,11 +1,19 @@
 let navBar = document.querySelectorAll('#nav');
 let backDiv = document.getElementById('back');
+let closeBackIcon = document.getElementById('back-close');
 let aboutSection = document.getElementById('about');
 let portfolioSection = document.getElementById('portfolio');
+
+const showInfoOnKeyDown = event => {
+  if (event.key === 'Enter') {
+    showInfo(event);
+  }
+}
 
 const showInfo = event => {
   if (backDiv) {
     backDiv.style.display = 'block';
+    closeBackIcon.setAttribute('tabindex', '0');
 
     if (event.target.className === 'about') {
       portfolioSection.style.display = 'none';
@@ -17,10 +25,18 @@ const showInfo = event => {
   }
 }
 
+const hideInfoOnKeyDown = event => {
+  if (event.key === 'Enter') {
+    hideInfo();
+  }
+}
+
 const hideInfo = () => {
+  closeBackIcon.setAttribute('tabindex', '-1');
   aboutSection.style.display = 'none';
   portfolioSection.style.display = 'none';
   backDiv.style.display = 'none';
 }
 
 navBar.forEach(link => link.addEventListener('click', showInfo));
+navBar.forEach(link => link.addEventListener('keydown', showInfoOnKeyDown));
