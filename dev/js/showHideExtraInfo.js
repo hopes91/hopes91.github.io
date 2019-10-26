e381ca2877b9fcdff75d40086f338bd66669558c
hopes91 <nadya.me91@gmail.com>
2019-10-26 12:34:36 +0300

    refactor the close icon classes

--- dev/js/showHideExtraInfo.js
+++ dev/js/showHideExtraInfo.js
@@ -1,6 +1,6 @@
 const navLinks = document.querySelectorAll('.nav li');
 const back = document.querySelector('.back');
-const closeBackIcon = document.querySelector('.back__close-icon');
+const closeBackIcon = document.querySelector('.close-icon');
 const portfolioSection = document.querySelector('.portfolio');
 const aboutSection = document.querySelector('.about');
 
