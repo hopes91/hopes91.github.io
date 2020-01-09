640f88c676001a0e75549a691e16117335c560a5
hopes91 <nadya.me91@gmail.com>
2020-01-09 12:32:34 +0300

    open project info in a pop up window

--- src/js/projectInfo.js
+++ src/js/projectInfo.js
@@ -1,4 +1,7 @@
 const projectsButtons = document.querySelectorAll('.project-tile-button');
+const windowBack = document.querySelector('.popup-window-back');
+const closeIcon = document.querySelector('.close-icon');
+const projectInfo = document.querySelectorAll('.project-info');
 
 const openProjectInfoOnKeyDown = event => {
   event.key === 'Enter' &&
@@ -6,17 +9,27 @@ const openProjectInfoOnKeyDown = event => {
 };
 
 const openProjectInfo = event => {
-  const projectClass = event.target.parentElement.parentElement.className;
-  const windowBack = document.querySelector('.popup-window-back');
-  const projectInfo = document.querySelectorAll('.project-info');
+  const projectClass = event.target.parentElement.parentElement.classList[1];
 
   projectInfo.forEach(info => {
-    if (info.className.match(projectClass)) {
+    if (info.classList[1].match(projectClass)) {
       windowBack.style.display = 'block';
       info.style.display = 'block';
     }
   });
 };
 
+const closeProjectInfoOnKeyDown = event => {
+  event.key === 'Escape' &&
+    closeProjectInfo();
+};
+
+const closeProjectInfo = () => {
+  projectInfo.forEach(info => info.style.display = 'none');
+  windowBack.style.display = 'none';
+};
+
 projectsButtons.forEach(button => button.addEventListener('keydown', openProjectInfoOnKeyDown));
 projectsButtons.forEach(button => button.addEventListener('click', openProjectInfo));
+window.addEventListener('keydown', closeProjectInfoOnKeyDown);
+closeIcon.addEventListener('click', closeProjectInfo);
