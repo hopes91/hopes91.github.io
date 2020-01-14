a38540a3cc9074d72580d44194d44b941f2c1f2f
hopes91 <nadya.me91@gmail.com>
2020-01-14 09:43:16 +0300

    add arrow up

---
+++ src/js/arrowUp.js
@@ -0,0 +1,15 @@
+const showHideArrowUp = () => {
+	const arrowUp = document.querySelector('.arrow-up');
+
+	if (window.pageYOffset > window.innerHeight / 3) {
+		arrowUp.style.display = 'inline-block';
+    arrowUp.setAttribute('tabindex', '0');
+	}
+
+	if (window.pageYOffset === 0) {
+		arrowUp.style.display = 'none';
+    arrowUp.setAttribute('tabindex', '-1');
+	}
+};
+
+window.addEventListener('scroll', showHideArrowUp);
