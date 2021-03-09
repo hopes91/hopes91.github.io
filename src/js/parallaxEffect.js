b92aeb30a519299fb927e3d81e9eaf1f5cbf5958
hopes91 <nadya.me91@gmail.com>
2021-03-09 11:29:48 +0300

    add a bigger rectangle to the parallax effect

--- src/js/parallaxEffect.js
+++ src/js/parallaxEffect.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('header');
 
 const handleParallaxEffect = event => {
-  const container = document.querySelector('.parallax-effect');
+  const wrapperSmall = document.querySelector('.wrapper-rectangle-small');
   const triangle = document.querySelector('.triangle');
   const circle1 = document.querySelector('.circle1');
   const rectangle = document.querySelector('.rectangle');
@@ -11,31 +11,29 @@ const handleParallaxEffect = event => {
 
 	let change;
 	let left = change * 20;
-	let xPos = event.clientX;
-	let yPos = event.clientY;
-	xPos = xPos * 2;
-	yPos = yPos * 2;
+	let cursorXPosition = event.clientX * 2;
+	let cursorYPosition = event.clientY * 2;
 
-  container.style.bottom = `${(0 + (yPos / 50))}px`;
-  container.style.right = `${(0 + (xPos / 50))}px`;
+  wrapperSmall.style.bottom = `${(0 + (cursorYPosition / 50))}px`;
+  wrapperSmall.style.right = `${(0 + (cursorXPosition / 50))}px`;
 
-  triangle.style.top = `${(0 - (yPos / 40))}px`;
-  triangle.style.left = `${(0 - (xPos / 30))}px`;
+  triangle.style.top = `${(0 - (cursorYPosition / 40))}px`;
+  triangle.style.left = `${(0 - (cursorXPosition / 30))}px`;
 
-  circle1.style.bottom = `${(0 + (yPos / 25))}px`;
-  circle1.style.left = `${(0 - (xPos / 35))}px`;
+  circle1.style.bottom = `${(0 + (cursorYPosition / 25))}px`;
+  circle1.style.left = `${(0 - (cursorXPosition / 35))}px`;
 
-  rectangle.style.top = `${(0 + (yPos / 20))}px`;
-  rectangle.style.right = `${(0 + (xPos / 30))}px`;
+  rectangle.style.top = `${(0 + (cursorYPosition / 20))}px`;
+  rectangle.style.right = `${(0 + (cursorXPosition / 30))}px`;
 
-  circle2.style.top = `${(0 - (yPos / 30))}px`;
-  circle2.style.right = `${(0 + (xPos / 30))}px`;
+  circle2.style.top = `${(0 - (cursorYPosition / 30))}px`;
+  circle2.style.right = `${(0 + (cursorXPosition / 30))}px`;
 
-  line1.style.bottom = `${(32 + (yPos / 50))}px`;
-  line1.style.right = `${(128 + (xPos / 50))}px`;
+  line1.style.bottom = `${(32 + (cursorYPosition / 50))}px`;
+  line1.style.right = `${(128 + (cursorXPosition / 50))}px`;
 
-  line1.style.bottom = `${(96 + (yPos / 50))}px`;
-  line1.style.right = `${(32 + (xPos / 50))}px`;
+  line2.style.bottom = `${(96 + (cursorYPosition / 50))}px`;
+  line2.style.right = `${(32 + (cursorXPosition / 50))}px`;
 };
 
 header.addEventListener('mousemove', handleParallaxEffect);
